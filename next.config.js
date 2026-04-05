be2a8263a817af93c53d338eb11e0dfdb66d2275
teoxboe <teoxboe@gmail.com>
2026-04-05 12:19:27 -0300

    fix: standalone output para melhorar performance

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
-/** @type {import('next').NextConfig} */
+//** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '*.supabase.co' },
-      { protocol: 'https', hostname: '*.supabase.in' },
-    ],
-  },
+  output: 'standalone',
 }
-
 module.exports = nextConfig
